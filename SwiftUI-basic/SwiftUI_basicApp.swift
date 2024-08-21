699ca713ebe21b10453f9560d1015188b6677867
dev-junehee <dev.junehee@gmail.com>
2024-08-21 09:45:50 +0900

    Initial Commit

---
+++ SwiftUI-basic/SwiftUI_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_basicApp.swift
+//  SwiftUI-basic
+//
+//  Created by junehee on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
